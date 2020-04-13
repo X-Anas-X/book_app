07ab933a69583502f0cffdd376283d9e019a1ed6
X-Anas-X <anas.zain1996@gmail.com>
2020-04-14 00:10:29 +0300

    done lab 11

--- server.js
+++ server.js
@@ -1,82 +1,210 @@
 'use strict';
-// request enviroment
+
 require('dotenv').config();
 
+//******************************************************* dependencies ***************************************************************
 
-//******************************************************* dependencies ***************************************************************\\
-
-
-// require the dependencies
 const express = require('express');
+const PORT = process.env.PORT || 3000;
 const superagent = require('superagent');
-const PORT = process.env.PORT || 4000;
-const app = express();
+const app   = express();
 
 
-app.set('view engine', 'ejs'); //this is for the html pages after installing it from the terminal
-app.use(express.static('./publics')); // the static css folders
+app.use('/public',express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({ extended: true })); // to get the data from the form
+//******************************************************* page routes ***************************************************************
 
-
-//******************************************************* rendering pages **************************************************************\\
-
-// rendering the html page
-app.get('/searches/new', (req, res)=>{
-//   console.log('this is getting from', req.query);
+app.get('/', (req, res) => {
+  res.render('./pages/index');
+});
+app.get('/search', (req,res)=>{
   res.render('pages/searches/new');
 });
-app.get('/hello', (req, res)=>{
-  res.render('pages/index');
-});
 
 
-//******************************************************* rendering superagent API *******************************************************\\
+//******************************************************* rendering the API ***************************************************************
 
-// using post to render the url
-app.post('/incoming', (req, res) =>{
+app.post('/searches', bookHandler); // calling th function here since it didnt waork in an arrow function.
 
-  let url; // declarinf the url variable to use it in the if statement
-  let newbookArr = [];
 
-  if (req.body.search === 'title'){
-    url = `https://www.googleapis.com/books/v1/volumes?q=${req.query.search}`;
-  }
-  else if (req.body.search === 'author'){
-    url = `https://www.googleapis.com/books/v1/volumes?q=${req.query.search}`;}
+function bookHandler (req, res){
+  let searchBook = req.body.search;
+  let checkGener = req.body.check;
 
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${checkGener}:${searchBook}`;
   superagent.get(url)
-    .then((key) => {
-      key.body.items.map( data =>{
-        const newBook = new Books(data);
-        newbookArr.push(newBook);
-      });
-      //   console.log(apiResponse.body.items[0]);
-      res.send(newbookArr);
+    .then(bookData =>
+      bookData.body.items.map((book)=>
+        new Books (book)))
+    .then((books)=> {
+      res.render('pages/searches/show' , {bookData:books});
+
     });
-});
+
+}
+
+// app.post('/searches', (req, res) =>{
+
+//   let searchBook = req.body.search;
+//   let checkGener = req.body.check;
+//   const url = `https://www.googleapis.com/books/v1/volumes?q=${checkGener}:${searchBook}`;
+//   superagent.get(url).then(bookData => {
+//     bookData.body.items.map(book =>{
+//       new Books(book)
+//         .then(books =>{
+//           res.render('pages/searches/show' , {bookData:books});
+//         });
+//     });
+//   });
+// });
 
 
-//******************************************************* constructor **********************************************************************\\
+function Books (value) {
+  this.images = value.volumeInfo.imageLinks.smallThumbnail;
+  this.title = value.volumeInfo.title;
+  this.authors = value.volumeInfo.authors;
+  this.description = value.volumeInfo.description;
 
-// constructor
-function Books(bookInfo){
-  this.title = bookInfo.volumeInfo.title;
-  this.authors = bookInfo.volumeInfo.authors[0];
-  this.description = bookInfo.volumeInfo.description;
-  this.image = bookInfo.volumeInfo.imageLinks.smallThumb;
 }
 
 
-//******************************************************* Error Handeling **********************************************************************\\
-// function errorHandler (err,req,res){
-//   res.status(500).send(err);
-// }
+//******************************************************* Error ***************************************************************\\
 
-app.get('*',(req,res)=>{
-  res.status(404).send('This route does not exist!!');
+app.use('*', (request, response) => {
+  response.render('pages/error');
+});
+
+app.listen(PORT, ()=>{
+
+  console.log(`listining to ${PORT}`);
+
 });
 
 
-app.listen(PORT, () => console.log('OK!'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////// failed attempt ///////////////////////////////////////////////////////////////
+
+
+
+
+
+
+// // request enviroment
+// require('dotenv').config();
+
+
+// //******************************************************* dependencies ***************************************************************
+
+// // require the dependencies
+// const express = require('express');
+// const superagent = require('superagent');
+// // const cors = require('cors');
+// const PORT = process.env.PORT || 4000;
+// const app = express();
+// // app.use(cors());
+
+
+// app.set('view engine', 'ejs'); //this is for the html pages after installing it from the terminal
+// // app.use(express.static('./publics')); // the static css folders
+// app.use('/public',express.static('public'));
+
+// app.use(express.urlencoded({ extended: true })); // to get the data from the form
+
+
+// //******************************************************* rendering pages **************************************************************\\
+
+// // rendering the html page
+// app.get('/', (req, res)=>{
+//   res.render('pages/index');
+// });
+// app.get('/hello', (req, res)=>{
+//   res.render('pages/index');
+// });
+// app.get('/show', (req, res)=>{
+//   res.render('pages/searches/show');
+// });
+// app.get('/searches/new', (req, res)=>{
+// //   console.log('this is getting from', req.query);
+//   res.render('pages/searches/new');
+// });
+
+
+// // app.get('/error', (req, res) => {
+// //   throw new Error('pages/error');
+// // });
+
+// //******************************************************* rendering superagent API *******************************************************\\
+// // let newBookArr = [];
+// // using post to render the url
+// app.post('/searches', (req, res) =>{
+
+//   let searchBook = req.body.search;
+//   let bookGener = req.body.check;
+
+//   // if (req.body.searchType === 'title'){
+//   //   url = `https://www.googleapis.com/books/v1/volumes?q=${req.body.yourBook}&intitle:${req.body.yourBook}`;
+//   // }
+//   // else if (req.body.searchType === 'author'){
+//   //   url = `https://www.googleapis.com/books/v1/volumes?q=${req.body.yourBook}+inauthor:${req.body.yourBook}`;}
+
+//   const url = `https://www.googleapis.com/books/v1/volumes?q=${bookGener}:${searchBook}`;
+
+//   superagent.get(url)
+//     .then((booksKey) => {
+//       booksKey.body.items.map(data =>{
+//         new Books(data);
+//         // newbookArr.push(newBook);
+//         res.render('pages/searches/show', {booksKey: data});
+//       });
+//     });
+// });
+
+
+// //******************************************************* constructor **********************************************************************\\
+
+// // constructor
+// function Books(bookInfo){
+//   this.title = bookInfo.volumeInfo.title;
+//   this.authors = bookInfo.volumeInfo.authors;
+//   this.description = bookInfo.volumeInfo.description;
+//   this.image = bookInfo.volumeInfo.imageLinks.smallThumb;
+// }
+
+
+// //******************************************************* Error Handeling **********************************************************************\\
+// // function errorHandler (err,req,res){
+// //   res.status(500).send(err);
+// // }
+
+// app.get('*',(req,res)=>{
+//   res.status(404).send('This route does not exist!!');
+// });
+
+
+// app.listen(PORT, () => console.log(`It's online time onnnnn ${PORT}!!!`));
+
+
+
 
